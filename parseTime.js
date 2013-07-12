c184994d7ddbe3bf4a67b741d01a4d93a872a0b9
Simon Waldherr <git@simon.waldherr.eu>
2013-07-13 01:37:28 +0200

    parse RFC2822 and ISO8601 better demo better code

--- parseTime.js
+++ parseTime.js
@@ -1,13 +1,13 @@
 /* * * * * * * * * *
  *  parseTime .js  *
- *  Version 0.1.2  *
+ *  Version 0.1.3  *
  *  License:  MIT  *
  * Simon  Waldherr *
  * * * * * * * * * */
 
 /*jslint browser: true, indent: 2 */
 
-var parseTime = function (string) {
+var parseTime = function (string, now) {
   "use strict";
   var re,
     lang,
@@ -15,127 +15,9 @@ var parseTime = function (string) {
     timedif,
     integer,
     unit,
+    words,
+    parsed,
     regex = {},
-    words = {
-      de: {
-        numbers: {
-          'null' : 0,
-          'ein' : 1,
-          'zwei' : 2,
-          'drei' : 3,
-          'ein paar' : 3.5,
-          'vier' : 4,
-          'fünf' : 5,
-          'sechs' : 6,
-          'sieben' : 7,
-          'acht' : 8,
-          'neun' : 9,
-          'zehn' : 10,
-          'elf' : 11,
-          'zwölf' : 12,
-          'dreizehn' : 13,
-          'vierzehn' : 14,
-          'fünfzehn' : 15,
-          'sechzehn' : 16,
-          'siebzehn' : 17,
-          'achzehn' : 18,
-          'neinzehn' : 19,
-          'zwanzig' : 20,
-          'dreißig' : 30,
-          'vierzig' : 40,
-          'fünfzig' : 50,
-          'sechzig' : 60,
-          'siebzig' : 70,
-          'achtzig' : 80,
-          'neunzig' : 90,
-          'hundert' : 100,
-          'tausend' : 1000,
-          'million' : 1000000
-        },
-        unit: {
-          'millisekunde' : 1,
-          'sekunde' : 1000,
-          'minute' : 60000,
-          'stunde' : 3600000,
-          'tag' : 8640000,
-          'woche' : 604800000,
-          'monat' : 2592000000,
-          'quartal' : 7776000000,
-          'jahr' : 31536000000,
-          'dekade' : 315360000000
-        },
-        fillwords: {
-          'vor' : '-',
-          'in' : '+'
-        },
-        fillfoo: {
-          '\\-' : '',
-          '\\ ' : '',
-          'e' : '',
-          'en' : '',
-          'er' : '',
-          'n' : ''
-        }
-      },
-      en: {
-        numbers: {
-          'zero' : 0,
-          'one' : 1,
-          'two' : 2,
-          'three' : 3,
-          'a few' : 3.5,
-          'four' : 4,
-          'five' : 5,
-          'six' : 6,
-          'seven' : 7,
-          'eight' : 8,
-          'nine' : 9,
-          'ten' : 10,
-          'eleven' : 11,
-          'twelve' : 12,
-          'thirteen' : 13,
-          'fourteen' : 14,
-          'fifteen' : 15,
-          'sixteen' : 16,
-          'seventeen' : 17,
-          'eighteen' : 18,
-          'nineteen' : 19,
-          'twenty' : 20,
-          'thirty' : 30,
-          'forty' : 40,
-          'fifty' : 50,
-          'sixty' : 60,
-          'seventy' : 70,
-          'eighty' : 80,
-          'ninety' : 90,
-          'hundred' : 100,
-          'thousend' : 1000,
-          'million' : 1000000
-        },
-        unit: {
-          'millisecond' : 1,
-          'second' : 1000,
-          'minute' : 60000,
-          'hour' : 3600000,
-          'day' : 8640000,
-          'week' : 604800000,
-          'month' : 2592000000,
-          'quarter' : 7776000000,
-          'year' : 31536000000,
-          'decade' : 315360000000
-        },
-        fillwords: {
-          'ago' : '-',
-          'in' : '+'
-        },
-        fillfoo: {
-          's' : '',
-          '\\-' : '',
-          '\\ ' : '',
-          '\\.' : ''
-        }
-      }
-    },
     adWordsToRegex = function (fillfoo, first) {
       var returnval = '', i;
       for (i in words[lang][fillfoo]) {
@@ -150,6 +32,136 @@ var parseTime = function (string) {
       }
       return returnval;
     };
+
+  now = parseInt(now, 10);
+  parsed = Date.parse(string);
+  if (!isNaN(parsed)) {
+    return {
+      'absolute': parsed,
+      'relative': (parsed - now)
+    };
+  }
+  regex = {};
+  words = {
+    de: {
+      numbers: {
+        'null' : 0,
+        'ein' : 1,
+        'zwei' : 2,
+        'drei' : 3,
+        'ein paar' : 3.5,
+        'vier' : 4,
+        'fünf' : 5,
+        'sechs' : 6,
+        'sieben' : 7,
+        'acht' : 8,
+        'neun' : 9,
+        'zehn' : 10,
+        'elf' : 11,
+        'zwölf' : 12,
+        'dreizehn' : 13,
+        'vierzehn' : 14,
+        'fünfzehn' : 15,
+        'sechzehn' : 16,
+        'siebzehn' : 17,
+        'achzehn' : 18,
+        'neinzehn' : 19,
+        'zwanzig' : 20,
+        'dreißig' : 30,
+        'vierzig' : 40,
+        'fünfzig' : 50,
+        'sechzig' : 60,
+        'siebzig' : 70,
+        'achtzig' : 80,
+        'neunzig' : 90,
+        'hundert' : 100,
+        'tausend' : 1000,
+        'million' : 1000000
+      },
+      unit: {
+        'millisekunde' : 1,
+        'sekunde' : 1000,
+        'minute' : 60000,
+        'stunde' : 3600000,
+        'tag' : 8640000,
+        'woche' : 604800000,
+        'monat' : 2592000000,
+        'quartal' : 7776000000,
+        'jahr' : 31536000000,
+        'dekade' : 315360000000
+      },
+      fillwords: {
+        'vor' : '-',
+        'in' : '+'
+      },
+      fillfoo: {
+        '\\-' : '',
+        '\\ ' : '',
+        'e' : '',
+        'en' : '',
+        'er' : '',
+        'n' : ''
+      }
+    },
+    en: {
+      numbers: {
+        'zero' : 0,
+        'one' : 1,
+        'two' : 2,
+        'three' : 3,
+        'a few' : 3.5,
+        'four' : 4,
+        'five' : 5,
+        'six' : 6,
+        'seven' : 7,
+        'eight' : 8,
+        'nine' : 9,
+        'ten' : 10,
+        'eleven' : 11,
+        'twelve' : 12,
+        'thirteen' : 13,
+        'fourteen' : 14,
+        'fifteen' : 15,
+        'sixteen' : 16,
+        'seventeen' : 17,
+        'eighteen' : 18,
+        'nineteen' : 19,
+        'twenty' : 20,
+        'thirty' : 30,
+        'forty' : 40,
+        'fifty' : 50,
+        'sixty' : 60,
+        'seventy' : 70,
+        'eighty' : 80,
+        'ninety' : 90,
+        'hundred' : 100,
+        'thousend' : 1000,
+        'million' : 1000000
+      },
+      unit: {
+        'millisecond' : 1,
+        'second' : 1000,
+        'minute' : 60000,
+        'hour' : 3600000,
+        'day' : 8640000,
+        'week' : 604800000,
+        'month' : 2592000000,
+        'quarter' : 7776000000,
+        'year' : 31536000000,
+        'decade' : 315360000000
+      },
+      fillwords: {
+        'ago' : '-',
+        'in' : '+'
+      },
+      fillfoo: {
+        's' : '',
+        '\\-' : '',
+        '\\ ' : '',
+        '\\.' : ''
+      }
+    }
+  };
 
   string = ' ' + string + ' ';
   for (lang in words) {
@@ -189,9 +201,16 @@ var parseTime = function (string) {
           unit = words[lang].unit[encoded[8].toLowerCase()];
           timedif = integer * unit;
           if (encoded.indexOf(words[lang].fillwords[0]) !== -1) {
-            timedif = (0 - timedif);
+            parsed = -timedif;
+            return {
+              'absolute': (now - timedif),
+              'relative': parsed
+            };
           }
-          return JSON.stringify([encoded, timedif]);
+          return {
+            'absolute': (now + timedif),
+            'relative': parsed
+          };
         }
       }
     }
