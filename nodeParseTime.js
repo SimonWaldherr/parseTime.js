75097ce564d84069ee584da95190913289dd4b6a
Simon Waldherr <git@simon.waldherr.eu>
2015-10-05 02:59:56 +0200

    jshint and new travis container-based infrastructure

--- nodeParseTime.js
+++ nodeParseTime.js
@@ -1,23 +1,26 @@
 var pt = require('./dist/parseTime.js'),
-  colors = require('colors'),
-  tests = []
-  fail = false;
+    colors = require('colors'),
+    tests = [],
+    fail = false;
 
 process.env.TZ = 'Europe/Berlin';
 
 tests = [
-  {'name': 'yesterday', 'input': 'yesterday', 'mode':'relative', 'output': -86400000},
-  {'name': 'birthday', 'input': '06.06.1989', 'mode':'absolute', 'output': 613087200000}];
+    {'name': 'yesterday', 'input': 'yesterday', 'mode':'relative', 'output': -86400000},
+    {'name': 'birthday', 'input': '06.06.1989', 'mode':'absolute', 'output': 613087200000},
+    {'name': '4thofjuly', 'input': 'am 4 Juli 2014', 'mode':'absolute', 'output': 1404424800000},
+    {'name': 'tomorrow', 'input': 'tomorrow', 'mode':'relative', 'output': 86400000}
+];
 
-for(i in tests) {
-  if(pt(tests[i].input)[tests[i].mode] === tests[i].output) {
-    console.log(tests[i].name.green);
-  } else {
-    console.log(tests[i].name.red + " should be: " + tests[i].output + " but is: " + pt(tests[i].input)[tests[i].mode]);
-    fail = true
-  }
+for(var i in tests) {
+    if(pt(tests[i].input)[tests[i].mode] === tests[i].output) {
+        console.log(tests[i].name.green);
+    } else {
+        console.log(tests[i].name.red + " should be: " + tests[i].output + " but is: " + pt(tests[i].input)[tests[i].mode]);
+        fail = true;
+    }
 }
 
-if(fail == true) {
-  process.exit(1);
+if(fail === true) {
+    process.exit(1);
 }
